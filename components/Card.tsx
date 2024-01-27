2d5729909bfcb06357fbc86c2ec08d580494d8e7
tomizdebski <tomizdebski@gmail.com>
2024-01-27 19:14:02 +0100

    fix_card_responsive

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,12 +3,13 @@ import React from "react";
 const Card = () => {
   return (
     <div
-      className="bg-[#ffffff] pt-[17px] pr-[19px] pb-[17px] pl-[19px] flex flex-col items-start justify-between shrink-0 w-[332px] h-[513px] relative shadow-2xl  shadow-gray-800"
+      className="bg-[#ffffff] pt-[17px] pr-[19px] pb-[17px] pl-[19px] flex flex-col items-start justify-between shrink-0 w-[332px] h-[513px] relative shadow-2xl  shadow-black
+      "
       
     >
       <div className="flex flex-row gap-[25px] items-center justify-start shrink-0 w-72 relative">
         <div className="bg-red-nav rounded-[5px] shrink-0 w-[13px] h-[91px] relative"></div>
-        <div className="text-grey text-left font-heading-2-font-family text-heading-2-font-size leading-heading-2-line-height font-heading-2-font-weight relative w-[250px]">
+        <div className="text-grey text-left font-saira text-huge relative w-[250px] leading-[105%]">
           Fully Customizable{" "}
         </div>
       </div>
--- components/CardsInfo.tsx
+++ components/CardsInfo.tsx
@@ -3,16 +3,16 @@ import Card from "./Card";
 
 const CardsInfo = () => {
   return (
-    <div className="flex flex-col items-center bg-white text-black pt-20 w-full gap-2">
+    <div className="flex flex-col items-center bg-white text-black py-20 w-full gap-10">
       <div className="text-grey text-left font-saira text-[50px] leading-[110%] font-bold relative">
         Advanced Statistics{" "}
       </div>
-      <div className="text-grey text-left font-inter  relative leading-3">
+      <div className="text-grey text-center font-inter  relative leading-5 ">
         Track how your links are performing across the web with our advanced
         statistics dashboard{" "}
       </div>
 
-      <div className="flex gap-3 flex-wrap px-auto justify-center">
+      <div className="flex gap-12 flex-wrap px-auto justify-center">
         <Card />
         <Card />
         <Card />
