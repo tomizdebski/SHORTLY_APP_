ec9caef248665293c2b127baf7d88f4e47a44b5b
tomizdebski <tomizdebski@gmail.com>
2024-02-01 21:51:59 +0100

    little_fix_layout

--- components/MainInput.tsx
+++ components/MainInput.tsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 const MainInput = () => {
   return (
     <div className="flex bg-white h-0 mx-auto w-[100%] justify-center">
-      <div className="xs:hidden sm:flex bg-black   rounded p-[34px]  flex-row  items-center  xs:w-[60vh] sm:w-[80%] max-w-[1080px] h-[111.8px] shadow-3xl  shadow-red-600 relative top-[-100px]  ">
+      <div className="xs:hidden sm:flex bg-black   rounded p-[34px]  flex-row  items-center  xs:w-[60vh] sm:w-[90%] max-w-[1080px] h-[111.8px] shadow-3xl  shadow-red-600 relative top-[-100px]  ">
         <div className="flex flex-col sm:flex-row items-center justify-between w-[100%] relative">
           <input className="bg-white rounded-none w-full h-12 relative"></input>
           <div className="flex flex-row gap-0 items-start justify-start shrink-0 relative">
