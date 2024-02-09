0ee2ec03893afc7542d8683e97dbe83c84b8cba2
tomizdebski <tomizdebski@gmail.com>
2024-02-09 19:44:44 +0100

    add_button_copy_to_clipboard

---
+++ components/CopyButton.tsx
@@ -0,0 +1,22 @@
+'use client';
+import React from 'react'
+
+const CopyButton = ({ text }: {text:String}) => {
+    const copyToClipboard = async () => {
+      try {
+        await navigator.clipboard.writeText(text as string);
+        alert('Link copied to clipboard!');
+      } catch (error) {
+        console.error('Failed to copy:', error);
+        alert('Failed to copy link!');
+      }
+    };
+  
+    return (
+      <button onClick={copyToClipboard} className='bg-soft-grey py-[12px] px-[88px] sm:py-[12px] sm:px-[24px]  flex-1 text-center  text-black  font-saira hover:bg-red-400'>
+        Copy
+      </button>
+    );
+  };
+  
+  export default CopyButton;
--- components/MainInput.tsx
+++ components/MainInput.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import Button from "./Button";
+import CopyButton from "./CopyButton";
 
 const MainInput = () => {
   return (
     <div className="flex bg-white h-0 mx-auto w-[100%] justify-center">
       <div className="xs:hidden sm:flex bg-black   rounded p-[34px]  flex-row  items-center  xs:w-[60vh] sm:w-[90%] max-w-[1080px] h-[111.8px] shadow-3xl  shadow-red-600 relative top-[-100px]  ">
         <div className="flex flex-col sm:flex-row items-center justify-between w-[100%] relative">
-          <input className="bg-white rounded-none w-full h-12 relative"></input>
+          <input className="bg-white rounded-none w-full h-12 relative">
+          
+          </input>
           <div className="flex flex-row gap-0 items-start justify-start shrink-0 relative">
+          <CopyButton text={"to jest link"}/>
             <Button
               text="Shorten It!"
               url="/signup"
               className="bg-red-nav py-[12px] px-[88px] sm:py-[12px] sm:px-[24px]  flex-1 text-center  text-[#ffffff]  font-saira hover:bg-red-400"
             />
+            
           </div>
         </div>
       </div>
@@ -26,7 +31,9 @@ const MainInput = () => {
               className="text-[#ffffff] text-left font-saira  relative  ">
             
           </Button>
+          
         </div>
+        
       </div>
     </div>
   );
