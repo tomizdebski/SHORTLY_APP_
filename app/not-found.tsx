5f4b79f9ab477a65f6d397ca2021efebbbb9e211
tomizdebski <tomizdebski@gmail.com>
2024-02-10 11:36:43 +0100

    improve_layout_page_not_found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found this is demo</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className='flex flex-col items-center justify-center h-[1000px] gap-4'>
+      <h2 className="text-[#ffffff] text-left font-saira ">Not Found this is demo</h2>
+      <p className="text-[#ffffff] text-left font-saira ">Could not find requested resource</p>
+      <Link className="main_btn" href="/">Return Home</Link>
     </div>
   )
 }
