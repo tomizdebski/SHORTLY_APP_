627d40884a488790d3a00f3d117ed9afaeeb01fd
tomizdebski <tomizdebski@gmail.com>
2024-02-17 17:06:49 +0100

    add_form_to_main_input_and_simple_validation

--- components/MainInput.tsx
+++ components/MainInput.tsx
@@ -33,21 +33,46 @@ const MainInput = ({ email }: any) => {
     await setUrl("");
   };
 
+  const isValidUrl = (url: string) => {
+    const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+    return urlRegex.test(url);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    setUrl(inputValue);
+  };
+
+  const handleSubmit = () => {
+    if (isValidUrl(url)) {
+      saveLinkToDb();
+    } else {
+      setUrl("This is not a valid URL. Please enter a valid URL.");
+    }
+  };
+
   return (
-    <div className="flex bg-white h-0 mx-auto w-[100%] justify-center">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}
+      className="flex bg-white h-0 mx-auto w-[100%] justify-center"
+    >
       <div className="xs:hidden sm:flex bg-black   rounded p-[34px]  flex-row  items-center  xs:w-[60vh] sm:w-[90%] max-w-[1080px] h-[111.8px] shadow-3xl  shadow-red-600 relative top-[-100px]  ">
         <div className="flex flex-col sm:flex-row items-center justify-between w-[100%] relative">
           <input
             className="bg-white text-black rounded-none w-full h-12 relative pl-3"
-            onChange={(ev) => setUrl(ev.target.value)}
+            onChange={(ev) => handleInputChange(ev)}
             type="text"
             placeholder="Shorten a link here..."
             value={url}
           />
+
           <div className="flex flex-row gap-0 items-start justify-start shrink-0 relative">
             <button
               className="bg-red-nav py-[12px] px-[88px] sm:py-[12px] sm:px-[24px]  flex-1 text-center  text-[#ffffff]  font-saira hover:bg-red-400"
-              onClick={() => saveLinkToDb()}
+              type="submit"
             >
               Shorten It!
             </button>
@@ -58,21 +83,20 @@ const MainInput = ({ email }: any) => {
       <div className="bg-black p-6 flex flex-col relative top-[-100px] shadow-3xl  shadow-red-600 h-[148px] w-[90%] sm:hidden rounded ">
         <input
           className="bg-[#ffffff] pl-3 text-black self-stretch shrink-0 h-12 relative overflow-hidden text-center"
-          onChange={(ev) => setUrl(ev.target.value)}
+          onChange={(ev) => handleInputChange(ev)}
           type="text"
           placeholder="Shorten a link here..."
           value={url}
         />
-        
-          <button
-            className="text-[#ffffff] text-left font-saira  relative bg-[rgba(228,2,2,0.94)] pt-3 pr-[97px] pb-3 pl-[97px] flex flex-col h-[48px] gap-3 items-center justify-center self-stretch   hover:bg-red-400"
-            onClick={() => saveLinkToDb()}
-          >
-            Shorten It!
-          </button>
-        
+
+        <button
+          className="text-[#ffffff] text-left font-saira  relative bg-[rgba(228,2,2,0.94)] pt-3 pr-[97px] pb-3 pl-[97px] flex flex-col h-[48px] gap-3 items-center justify-center self-stretch   hover:bg-red-400"
+          type="submit"
+        >
+          Shorten It!
+        </button>
       </div>
-    </div>
+    </form>
   );
 };
 
